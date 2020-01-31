44ed328fabe6874cbb05f3a705bd1fe8896c75a2
yuhaiyang1 <18771350464@163.com>
2020-01-31 17:26:05 +0800

    第一次提交
    第一次提交

---
+++ precache-manifest.c037fec6370479a1471f255642412bf2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e27737edf63a057a3b09f00d0a09e1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08fbbe04ff039c62eff",
+    "url": "/static/css/2.c35655eb.chunk.css"
+  },
+  {
+    "revision": "5dca0a769fd442b8f0a4",
+    "url": "/static/css/main.f5df3872.chunk.css"
+  },
+  {
+    "revision": "e08fbbe04ff039c62eff",
+    "url": "/static/js/2.f5238c49.chunk.js"
+  },
+  {
+    "revision": "512d6ec5c039ea53590f22c725b1d0e1",
+    "url": "/static/js/2.f5238c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dca0a769fd442b8f0a4",
+    "url": "/static/js/main.d49ccf85.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "8dfbca4f840463f6bcfb1df13edb7cfc",
+    "url": "/static/media/app.8dfbca4f.less"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
